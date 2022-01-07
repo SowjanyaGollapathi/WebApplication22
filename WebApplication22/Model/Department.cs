4192089e286a6ef6c61300048498a6150f422eed
sowjanya.gollapathi <sowjanya.gollapathi@gmail.com>
2022-01-07 22:00:13 +0530

    Add project files.

---
+++ WebApplication22/Model/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication22.Model
+{
+    public class Department
+    {
+        public int ID { get; set; }
+        public string DepartmentName { get; set; }
+        public string Course { get; set; }
+        public string Location { get; set; }
+
+    }
+}
